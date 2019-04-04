cfb76a0e3770b5fb391fcd90e31d32d6114a96b7
KnightRider <omar.brown@comcast.net>
2019-04-04 18:07:53 -0400

    first changes

--- pixart.js
+++ pixart.js
@@ -1,1 +1,16 @@
+let button= document.getElementById('set-color');
+let brush= document.getElementsByClassName('brush')[0];
+let input= document.getElementById("color-field");
 
+button.addEventListener('click', function(){
+    event.preventDefault()
+   brush.style.backgroundColor = input.value
+})
+
+
+ for(let i = 0; i < 20; i++){
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('square');
+    document.body.appendChild(newDiv);
+newDiv.addEventListen('click', )
+ }
